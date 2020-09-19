f9ff08208760fcec46b473f2e6b8f2db1c5449c4
boandriiv <bo.andriiv@gmail.com>
2020-09-19 11:18:51 +0300

    fixes

--- js/app.js
+++ js/app.js
@@ -2,9 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Toggle hamburger active class
     let hamburger = document.querySelector(".hamburger");
+    let body = document.querySelector("body");
 
     hamburger.addEventListener("click", function() {
         hamburger.classList.toggle("is-active");
+        body.classList.toggle("nav-is-open");
     });
 
 });
