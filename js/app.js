e5de54f32792a1f72aca8599c100ed7af5f302af
boandriiv <bo.andriiv@gmail.com>
2020-09-19 20:29:27 +0300

    fixes

--- js/app.js
+++ js/app.js
@@ -9,4 +9,46 @@ document.addEventListener("DOMContentLoaded", function() {
         body.classList.toggle("nav-is-open");
     });
 
+    //Initialize controller for scrollmagic
+    var controller = new ScrollMagic.Controller();
+
+    //Images animation on scroll
+    var revealItem = document.getElementsByClassName('scrl');
+    for (var i = 0; i < revealItem.length; i++) {
+        if (window.innerWidth >= 768) {
+            new ScrollMagic.Scene({
+                triggerElement: revealItem[i],
+                offset: 0,
+                triggerHook: 0.75,
+            })
+                .setClassToggle(revealItem[i], '_visible')
+                .addTo(controller);
+        } else if (window.innerWidth < 768) {
+            new ScrollMagic.Scene({
+                triggerElement: revealItem[i],
+                offset: 0,
+                triggerHook: 0.85,
+            })
+                .setClassToggle(revealItem[i], '_visible')
+                .addTo(controller);
+        }
+    }
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 150) {
+            $(".top-nav").addClass("top-nav_solid");
+        } else {
+            $(".top-nav").removeClass("top-nav_solid");
+        }
+    });
+
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1000);
+    });
 });
